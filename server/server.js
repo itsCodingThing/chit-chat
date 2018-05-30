8d0adce56f2f401bcaf38693b6cfae21dfd5d4b5
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2018-05-31 00:08:06 +0530

    emit broadcast message

--- server/server.js
+++ server/server.js
@@ -25,10 +25,18 @@ io.on('connection', (socket) => {
 
     socket.on('createMessage', (message) => {
         console.log(`createdMessage: ${message.from},${message.text}`);
+    /*
         io.emit('newMessage', {
             form: message.from,
             text: message.text,
             createdAt: new Date().getTime()
-        });      
+        });
+    */      
+
+        socket.broadcast.emit('newMessage', {
+            from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+        });
     })
 });
